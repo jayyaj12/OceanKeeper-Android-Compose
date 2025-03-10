e5c674492d1dc3f5346519927d2a89ab5b8c3efa
jayyaj12 <arbert12@naver.com>
2025-03-11 00:35:16 +0900

    [FEAT#1] UiEventHandler 추가

---
+++ presentation/src/main/java/com/aos/oceankeeper_android_compose/ui/UiEventHandler.kt
@@ -0,0 +1,126 @@
+package com.aos.oceankeeper_android_compose.ui
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
+import com.example.oceankeeper_android_compose.R
+import androidx.core.graphics.toColorInt
+import com.aos.oceankeeper_android_compose.base.BaseViewModel
+import kotlinx.coroutines.delay
+import timber.log.Timber
+
+object UiEventHandler {
+
+    @Composable
+    fun EventUi(event: BaseViewModel.Event) {
+        var isLoading by remember { mutableStateOf(false) }
+        var toastText by remember { mutableStateOf("") }
+        var isToastError by remember { mutableStateOf(false) }
+
+        LaunchedEffect(event) {
+            Timber.e("event $event")
+
+            when (event) {
+                is BaseViewModel.Event.ShowToast -> {
+                    toastText = event.message
+                    isToastError = false
+                    delay(2000)
+                    toastText = ""
+                }
+                is BaseViewModel.Event.ShowErrorToast -> {
+                    isLoading = false
+                    toastText = event.message
+                    isToastError = true
+                    delay(2000)
+                    toastText = ""
+                }
+                is BaseViewModel.Event.ShowToastRes -> toastText = event.message.toString()
+                is BaseViewModel.Event.ExpiredToken -> TODO()
+                is BaseViewModel.Event.ShowLoading -> isLoading = true
+                is BaseViewModel.Event.HideLoading -> isLoading = false
+                is BaseViewModel.Event.Nothing -> {}
+            }
+        }
+
+        LoadingUi(isLoading)
+        ToastUi(toastText, isToastError)
+    }
+
+    @Composable
+    private fun LoadingUi(isLoading: Boolean) {
+        if (isLoading) {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                CircularProgressIndicator()
+            }
+        }
+    }
+
+    @Composable
+    private fun ToastUi(text: String, isToastError: Boolean) {
+        var isVisible by remember { mutableStateOf(false) }
+
+        LaunchedEffect(text) {
+            isVisible = text.isNotBlank()
+        }
+
+        AnimatedVisibility(
+            visible = isVisible,
+            enter = fadeIn(tween(500)),
+            exit = fadeOut(tween(500))
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(bottom = 84.dp),
+                verticalAlignment = Alignment.Bottom,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Image(painter = painterResource(id = if(isToastError) {
+                    R.drawable.error_icon
+                } else {
+                    R.drawable.success_icon
+                }), contentDescription = "toast icon")
+                Text(
+                    modifier = Modifier
+                        .size(320.dp, 72.dp)
+                        .background(color = Color("#99000000".toColorInt()))
+                        .clip(RoundedCornerShape(8.dp))
+                        .wrapContentHeight(Alignment.CenterVertically),
+                    text = text,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
+}
